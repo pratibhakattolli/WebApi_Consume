e452999fcaa021a8944f3fdf87d2c4df27d3d86b
pkattolli <pkattolli@US-PF399T8E>
2022-03-21 07:36:31 -0700

    Add project files.

---
+++ Controllers/CrudController.cs
@@ -0,0 +1,43 @@
+﻿using ClassLibrary1;
+using Library2.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_Consume.Controllers
+{
+    [Authorize]
+    public class CrudController : Controller
+    {
+        private readonly Interface_Api_Service db;
+        public CrudController(Interface_Api_Service Db)
+        {
+            db = Db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var list = await db.GetAllAsync();
+            return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Invest pd)
+        {
+            if (ModelState.IsValid)
+            {
+                bool resp = await db.AddAsync(pd);
+                if (resp)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(pd);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary1;
+
+namespace WebApi_Consume.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ClassLibrary1.Invest> Invest { get; set; }
+    }
+}
